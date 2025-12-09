f92d218f799561451256437fd2852f4fd97c13d0
jonah <mail@jonahv.com>
2025-12-09 16:15:06 +0000

    agentic DESTINY search
    previously, we only had a DESTINY paper helper that could download the first page of references that resulted from an LLM generated search query
    
    now, we have a dedicated DESTINY paper search tool an agent can use to source information for its other steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -165,6 +165,23 @@ def agent_test_settings(agent_index_dir: Path, stub_data_dir: Path) -> Settings:
     settings.answer.evidence_k = 10
     return settings
 
+@pytest.fixture
+def azure_agent_test_settings(agent_index_dir: Path, stub_data_dir: Path) -> Settings:
+    # Lazily import from paperqa so typeguard doesn't throw:
+    # > /path/to/.venv/lib/python3.12/site-packages/typeguard/_pytest_plugin.py:93:
+    # > InstrumentationWarning: typeguard cannot check these packages because they
+    # > are already imported: paperqa
+    from paperqa.settings import Settings
+
+    # NOTE: originally here we had usage of embedding="sparse", but this was
+    # shown to be too crappy of an embedding to get past the Obama article
+    settings = Settings.from_name("azure")
+    settings.agent.index.paper_directory = stub_data_dir
+    settings.agent.index.index_directory = agent_index_dir
+    settings.agent.search_count = 10
+    settings.answer.answer_max_sources = 10
+    settings.answer.evidence_k = 10
+    return settings
 
 @pytest.fixture
 def agent_stub_session() -> PQASession:
